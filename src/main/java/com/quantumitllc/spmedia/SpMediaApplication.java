1ab631464199ccadcfde0f54b4899b8ea81b81ed
Ismoil <ismoil@kali>
2023-12-06 03:06:18 +0500

    feat: feat new configurations

---
+++ src/main/java/com/quantumitllc/spmedia/SpMediaApplication.java
@@ -0,0 +1,13 @@
+package com.quantumitllc.spmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpMediaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpMediaApplication.class, args);
+  }
+
+}
